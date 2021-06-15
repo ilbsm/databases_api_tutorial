de7c599df1e0b654d7faf9051cd9dfc4ba23d46f
Paweł Rubach <pawel.rubach@gmail.com>
2021-06-15 14:33:34 +0200

    LassoProt - download all files for protein and chain

--- lassoprot_api.py
+++ lassoprot_api.py
@@ -3,12 +3,8 @@ from database_downloader import DatabaseDownloader
 SEARCH_STRING = 'https://lassoprot.cent.uw.edu.pl/browse/?lassoType=L3&set=0&bridgeType=ssbridge%2Camide%2Cester%2Cthioester%2Cothers&array=0&raw=1'
 
 
-'https://lassoprot.cent.uw.edu.pl/static/lasso_data/6S24/A/surface_6S24_A_399_480.tcl'
+URL_ALL_FILES = 'https://lassoprot.cent.uw.edu.pl/files/{0}_{1}_all.tar.bz2'
 
 
-dd = DatabaseDownloader(SEARCH_STRING, [URL_KNOT_MATRIX,
-                                        URL_KNOT_MATRIX_SVG,
-                                        URL_KNOT_MATRIX_ARROWS_SVG,
-                                        URL_KNOTMAP,
-                                        URL_TRIMMED_PDB,
-                                        URL_CHAIN_XYZ], 'out_dir', create_separate_dirs=False)
+dd = DatabaseDownloader(SEARCH_STRING, [URL_ALL_FILES], 'out_dir', create_separate_dirs=False)
+dd.get_all()
